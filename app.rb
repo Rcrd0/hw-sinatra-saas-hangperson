89f3580ad0219cf2674c1723e4293557fccbd3ad
Rcrd0 <ricardo.panero@gmail.com>
2015-11-07 23:25:41 +0000

    No cheating

--- app.rb
+++ app.rb
@@ -73,13 +73,21 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
     #redirect '/win'
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
     #redirect '/lose'
   end
   
